2da4dfdad6167804221f4b92227caf27d6adbeea
Charles Clemens <cclemensjr@gmail.com>
2019-01-11 10:38:26 -0800

    Output board to Console

--- Lab04_TicTacToe/Classes/Board.cs
+++ Lab04_TicTacToe/Classes/Board.cs
@@ -19,9 +19,17 @@ namespace Lab04_TicTacToe.Classes
 
 		public void DisplayBoard()
 		{
-			
-			//TODO: Output the board to the console
-		
-		}
+
+            //TODO: Output the board to the console
+            for (int i = 0; i < GameBoard.Length - 1; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"|{GameBoard[i, j]}|");
+                }
+
+                Console.WriteLine();
+            }
+        }
 	}
 }
